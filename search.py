f2d15293b9053179b2c811270063dc4c86a3e87f
Mikhailo Kisil <mihajlokisil20@gmail.com>
2023-08-03 17:13:55 +0300

    The output of the result has been changed

--- search.py
+++ search.py
@@ -9,33 +9,32 @@ morph = pymorphy2.MorphAnalyzer(lang='uk')
 
 
 def search_keywords(k_words, text):
-    dct_kwords_count = {}
+    count_kwords = 0
     for kw in k_words:
         normal_form_kw = morph.parse(kw)[0].normal_form
-        dct_kwords_count[kw] = 0
         for w in text:
             if fuzz.ratio(kw, w) > 65:
                 normal_form_w = morph.parse(w)[0].normal_form
                 if bool(re.match(rf'{kw}?[а-яіїєґ]{{,3}}', w)) or bool(re.match(rf'{w}[а-яіїєґ]{{,3}}\b', kw)):
-                    dct_kwords_count[kw] += 1
+                    count_kwords += 1
                     delete_word_from_text(w, text)
                 elif ('не' in w[:2] or 'не' in kw[:2]) and not ('не' in w[:2] and 'не' in kw[:2]):
                     continue
                 elif bool(re.match(rf'{normal_form_kw}?[а-яіїєґ]{{,3}}', normal_form_w)):
-                    dct_kwords_count[kw] += 1
+                    count_kwords += 1
                     delete_word_from_text(w, text)
                 elif fuzz.ratio(normal_form_kw, normal_form_w) >= 80:
-                    dct_kwords_count[kw] += 1
+                    count_kwords += 1
                     delete_word_from_text(w, text)
                 elif fuzz.ratio(kw, w) >= 80:
-                    dct_kwords_count[kw] += 1
+                    count_kwords += 1
                     delete_word_from_text(w, text)
 
-    return dct_kwords_count
+    return count_kwords
 
 
 def search_keyphrases(k_phrases, text):
-    dct_kwords_count = {}
+    count_kphrases = 0
     for ph in k_phrases:
         updated_text, phrases_cnt = re.subn(rf'\b{ph[:-1]}?[а-яіїєґ]{{,2}}', '', text)
         text = updated_text
@@ -46,9 +45,9 @@ def search_keyphrases(k_phrases, text):
             if fuzz.ratio(ph, text_slice) >= 85:
                 phrases_cnt += 1
 
-        dct_kwords_count[ph] = phrases_cnt
+        count_kphrases += phrases_cnt
 
-    return dct_kwords_count
+    return count_kphrases
 
 
 def delete_word_from_text(w, text):
@@ -65,11 +64,38 @@ def prepare_text(text, symbols_to_delete):
     return text
 
 
+def get_color_keywords(cnt_keywords, colors):
+    for c in colors:
+        diapason = c.split('-')[0].split(',')
+        if len(diapason) == 2:
+            if int(diapason[0]) <= cnt_keywords <= int(diapason[1]):
+                return c.split('-')[1]
+        elif len(diapason) == 1:
+            if int(diapason[0]) <= cnt_keywords:
+                return c.split('-')[1]
+
+
 def search():
     keywords_files = os.listdir('keywords')
     conversations_files = os.listdir('conversations')
 
-    result = {}
+    while True:
+        filename = input('Введіть назву файлу із якого потрібно почати(наприклад ria_00.txt): ')
+        if filename in conversations_files:
+            index_filename = conversations_files.index(filename)
+            conversations_files = conversations_files[index_filename:]
+            break
+        else:
+            print('Помилка. Введіть правильну назву файлу')
+
+    if filename == keywords_files[0]:
+        result = {}
+        result['keywords_files'] = [file.split('.')[0] for file in keywords_files]
+        result['conversations_files'] = {}
+    else:
+        with open('result.json', encoding='utf-8') as file:
+            result = json.loads(file.read())
+
     for conversation_file in conversations_files:
         print(f'Обробка файлу {conversation_file}')
         with open(f'conversations/{conversation_file}', 'r', encoding='utf-8') as file:
@@ -78,19 +104,24 @@ def search():
         text_for_search_phrases = " ".join(conversation_text)
         text_for_search_words = [w for w in conversation_text if len(w) > 2]
 
-        result[conversation_file] = {}
-        for keywords_file in keywords_files:
-            with open(f'keywords/{keywords_file}', 'r', encoding='utf-8') as file:
-                keywords = prepare_text(file.read(), '-,.?!;:…_«»*').split('\n')
+        result['conversations_files'][conversation_file] = {}
+        for keyword_file in keywords_files:
+            with open(f'keywords/{keyword_file}', 'r', encoding='utf-8') as file:
+                file_text = file.read()
+                colors = file_text.strip().split('\n')[0].split()
+                keywords = prepare_text(file_text, '-,.?!;:…_«»*').split('\n')[1:]
                 k_words = set([i for i in keywords if ' ' not in i])
                 k_phrases = set([i for i in keywords if ' ' in i])
 
-            found_words = search_keywords(k_words, text_for_search_words)
-            found_phrases = search_keyphrases(k_phrases, text_for_search_phrases)
+            count_kwords = search_keywords(k_words, text_for_search_words)
+            count_kphrases = search_keyphrases(k_phrases, text_for_search_phrases)
 
-            found_words.update(found_phrases)
-
-            result[conversation_file][keywords_file] = found_words
+            count = count_kwords + count_kphrases
+            result['conversations_files'][conversation_file][keyword_file] = {
+                "count": count,
+                "len_text": len(conversation_text),
+                "color": get_color_keywords(count, colors)
+            }
 
         with open('result.json', 'w', encoding='utf-8') as file:
             json.dump(result, file, indent=2, ensure_ascii=False)
@@ -102,27 +133,13 @@ def generate_html():
     with open('result.json', encoding='utf-8') as file:
         data = json.loads(file.read())
 
-    template = jinja2.Template("""\
-        {% for file in data %}
-            <h1>{{ file }}</h1>
-            <ul>
-                {% for keywords_file in data[file] %}
-                    <li>
-                        <h2>{{ keywords_file }}</h2>  
-                        <ul>
-                            {% for keyword in data[file][keywords_file] %}
-                                <li>{{ keyword }}: {{ data[file][keywords_file][keyword] }}</li>
-                            {% endfor %}
-                        </ul>
-                    </li>
-                {% endfor %}
-            </ul>
-        {% endfor %}
-        """)
+    with open('result/template.html', encoding='utf-8') as file:
+        html_template = file.read()
 
+    template = jinja2.Template(html_template)
     html = template.render(data=data)
 
-    with open('result.html', 'w', encoding='utf-8') as file:
+    with open('result/result.html', 'w', encoding='utf-8') as file:
         file.write(html)
 
 
